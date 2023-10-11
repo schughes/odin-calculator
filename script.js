f551d45d4b3df675ec18923870f46b9d0e115e0a
Samantha Hughes <samantha.hughes@quantumworkplace.com>
2023-10-11 11:24:46 -0500

    add numbers to display

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+function showValue() {
+  let val = this.getAttribute("val");
+  let display = document.querySelector("#display");
+  display.textContent += val;
+}
 document.addEventListener("DOMContentLoaded", function (event) {
   var body = document.querySelector("body");
   body.classList.remove("preload");
   var body = document.querySelector("body");
 
-  //   var buttons = document.querySelectorAll(".button");
-  //   console.log(buttons);
-  //   buttons.forEach((button) => {
-  //     var screen = document.createElement("div");
-  //     screen.classList.add("screen");
-  //     button.appendChild(screen);
-  //   });
+  var buttons = document.querySelectorAll(".button .screen");
+  //   console.log(numbers);
+  buttons.forEach((button) => {
+    let value = button.querySelector("h1").textContent;
+    button.setAttribute("val", value);
+    button.addEventListener("click", showValue);
+  });
 });
