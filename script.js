10e7bb33e7f3498e734a5c13ac0224a4b9e81d2a
Samantha Hughes <samantha.hughes@quantumworkplace.com>
2023-10-11 10:17:21 -0500

    get animations working correctly

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  var body = document.querySelector("body");
+  body.classList.remove("preload");
+  var body = document.querySelector("body");
+
+  //   var buttons = document.querySelectorAll(".button");
+  //   console.log(buttons);
+  //   buttons.forEach((button) => {
+  //     var screen = document.createElement("div");
+  //     screen.classList.add("screen");
+  //     button.appendChild(screen);
+  //   });
+});
