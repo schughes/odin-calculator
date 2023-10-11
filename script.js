c9b2d85bd7a802cc325ea6df7b030e3593198489
Samantha Hughes <samantha.hughes@quantumworkplace.com>
2023-10-11 11:27:59 -0500

    clear display

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ function showValue() {
   let display = document.querySelector("#display");
   display.textContent += val;
 }
+
+function clearDisplay() {
+  let display = document.querySelector("#display");
+  display.textContent = "";
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
   var body = document.querySelector("body");
   body.classList.remove("preload");
   var body = document.querySelector("body");
 
   var buttons = document.querySelectorAll(".button .screen");
-  //   console.log(numbers);
   buttons.forEach((button) => {
     let value = button.querySelector("h1").textContent;
     button.setAttribute("val", value);
     button.addEventListener("click", showValue);
   });
+
+  // special buttons
+  var c = document.querySelector("#c");
+  c.removeEventListener("click", showValue);
+  c.addEventListener("click", clearDisplay);
 });
